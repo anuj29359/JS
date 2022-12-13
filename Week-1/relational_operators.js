3e8ba3d2e2e8fe4b8f0940ba35a8e83208438b8e
anuj29359 <anuj293@gmail.com>
2022-12-13 15:26:14 +0530

    added loose/strict equality code

--- Week-1/relational_operators.js
+++ Week-1/relational_operators.js
@@ -4,9 +4,9 @@
 // Print true if first number is greater then second, else print false.
 // Print whether second number is greater or not.
 
-var num1 = 40, num2= 58;
-var fname = "Anuj"
-var num1_clone = 40;
+let num1 = 40, num2= 58;
+let fname = "Anuj"
+let num1_clone = 40;
 
 function is_equal(num1, num2){
     if(num1==num2){
@@ -17,12 +17,14 @@ function is_equal(num1, num2){
 }
 
 function is_same_datatype(var1, var2){
-    if(num1===num2){
+    if(var1===var2){
         console.log("same data type")
     }
     else
     console.log("different datatype")
 }
+
+console.log("comparing loose equality")
 
 is_equal(num1, num2);
 is_equal(num1,num1_clone)
@@ -32,3 +34,6 @@ is_equal(num1, fname)
 is_same_datatype(num1, num2);
 is_same_datatype(num1,num1_clone)
 is_same_datatype(num1, fname)
+
+console.log("comparing loose equality", 5=='5')
+console.log("strict equality", num1===num1_clone)
