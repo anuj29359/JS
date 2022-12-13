12a0b73df021e8ac3630fb6d97fd6d23e7b1ad94
anuj29359 <anuj293@gmail.com>
2022-12-13 15:33:35 +0530

    tried 'use strict'

---
+++ Week-1/variable_typo_mistake.js
@@ -0,0 +1,19 @@
+
+
+"use strict";
+
+
+var marks = 1
+var grade;
+switch(marks){
+    case 1:
+        grade='fail'
+        break;
+    case 2:
+        grade='pass';
+        break;
+    default:
+        grade='na'
+}
+
+console.log(grade)
