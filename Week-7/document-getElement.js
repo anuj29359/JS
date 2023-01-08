fa39e3389c92ce18373e169cb08286eb7dcbf981
anuj29359 <anuj293@gmail.com>
2023-01-08 15:01:32 +0530

    updated

---
+++ Week-7/document-getElement.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("h001"));
+console.log(document.getElementById("p001"));
+console.log(document.getElementById("p002"));
+
+console.log(document.getElementByClass("para"))
